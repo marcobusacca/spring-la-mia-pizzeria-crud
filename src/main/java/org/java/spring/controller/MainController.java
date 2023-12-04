319e8dc28cd01b60249081f4de76d32e5fac045a
marcobusacca <marcobusacca01@gmail.com>
2023-12-04 11:36:23 +0100

    [FEAT] MainController function storePizza

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -7,9 +7,14 @@ import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -48,5 +53,20 @@ public class MainController {
 		model.addAttribute("pizza", pizza);
 		
 		return "pizza-form";
+	}
+	@PostMapping("/pizzas/create")
+	public String storePizza(Model model, @Valid @ModelAttribute Pizza pizza, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			
+			System.out.println(bindingResult);
+			
+			model.addAttribute("pizza", pizza);
+			return "pizza-form";
+		}
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/";
 	}
 }
